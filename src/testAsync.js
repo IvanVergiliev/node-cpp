d69b009485f5aea7f5b6fef5018799f85953b000
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2013-05-17 14:04:56 +0300

    Last tweaks before the talk.

--- src/testAsync.js
+++ src/testAsync.js
@@ -2,6 +2,11 @@ var async = require('./build/Release/async');
 var a = require('./utils').randomArray(1000000);
 
 console.time('asyncSort');
+console.time('afterSort');
+setTimeout(function () {
+  console.log('after sort');
+  console.timeEnd('afterSort');
+}, 0);
 async.sort(a, function (res) {
   console.timeEnd('asyncSort');
   console.log(res.slice(0, 10));
